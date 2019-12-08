5668833e336e5f4c65946d59e54d3d7bd067a339
hadi-ilies.bereksi-reguig <hadi-ilies.bereksi-reguig@protonmail.com>
2019-12-08 19:41:00 +0100

    doc(doc): i have added 2 CPU doc

--- src/constant/constants.go
+++ src/constant/constants.go
@@ -2,11 +2,8 @@ package constant
 
 // constants.go file
 
+//all function and const that start with an upper case is "public" in golang
 const (
 	ExitSuccess = 0
 	ExitFailure = 1
 )
-
-func Lol() string {
-	return "LOOOL"
-}
