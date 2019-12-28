0553193f3ead118266ae742bd8c6ecdce0cc2a71
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2019-12-28 15:06:49 +0100

    feat(ui/openglencapsulation): package creation that contain all the encapsulation of opengl function

---
+++ src/ui/openglencapsulation/textures.go
@@ -0,0 +1,16 @@
+package openglencapsulation
+
+import "github.com/go-gl/gl/v2.1/gl"
+
+func CreateTexture() uint32 {
+	var texture uint32
+
+	gl.GenTextures(1, &texture)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	gl.BindTexture(gl.TEXTURE_2D, 0)
+	return texture
+}
