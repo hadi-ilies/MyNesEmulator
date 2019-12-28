06be3e3f485849f99cb3471104bccb1492ab81c9
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2019-12-28 16:54:41 +0100

    feat(ui/views): game view almost finished have to finish drawbuffer

--- src/ui/views/game.go
+++ src/ui/views/game.go
@@ -3,14 +3,18 @@ package views
 import (
 	"image"
 
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/hadi-ilies/MyNesEmulator/src/ui"
+	oglEncap "github.com/hadi-ilies/MyNesEmulator/src/ui/openglencapsulation" // import and rename the package openglencapsulation to oglEncap
 )
 
 type GameView struct {
-	nes     ui.Nes
-	ui      ui.Ui // lol there is no inerittance in golang, I am a noob ':(
+	nes     *ui.Nes
+	ui      *ui.Ui // lol there is no inerittance in golang, I am a noob ':(
 	texture uint32
 	frames  []image.Image
+	record  bool // i am recording ?
 	// director *Director
 	// console  *nes.Console
 	// title    string
@@ -18,4 +22,120 @@ type GameView struct {
 	// texture  uint32
 	// record   bool
 	// frames   []image.Image
+}
+
+func NewGameView(ui *ui.Ui, nes *ui.Nes) ui.View {
+	var gameView GameView
+
+	gameView.texture = oglEncap.CreateTexture()
+	gameView.nes = nes
+	gameView.ui = ui
+	return &gameView
+}
+
+func (gameView *GameView) Start() {
+	gl.ClearColor(0, 0, 0, 1)
+	// view.director.SetTitle(view.title)
+	// view.console.SetAudioChannel(view.director.audio.channel)
+	// view.console.SetAudioSampleRate(view.director.audio.sampleRate)
+	gameView.ui.GetWindow().SetKeyCallback(gameView.onKey)
+	// load state
+	// if err := view.console.LoadState(savePath(view.hash)); err == nil {
+	// 	return
+	// } else {
+	gameView.nes.Reset() //init nes
+	// //}
+	// // load sram
+	//cartridge := gameView.nes.GetComponents().GetCartridge()
+	// if cartridge.Battery != 0 {
+	// 	if sram, err := readSRAM(sramPath(gameView.hash)); err == nil {
+	// 		cartridge.SRAM = sram
+	// 	}
+	// }
+}
+
+func (gameView *GameView) Update() {
+	// if dt > 1 {
+	// 	dt = 0
+	// }
+	//window := view.director.window
+	// console := view.console
+	// if joystickReset(glfw.Joystick1) {
+	// 	view.director.ShowMenu()
+	// }
+	// if joystickReset(glfw.Joystick2) {
+	// 	view.director.ShowMenu()
+	// }
+	// if readKey(window, glfw.KeyEscape) {
+	// 	view.director.ShowMenu()
+	// }
+	//updateControllers(window, console)
+	//console.StepSeconds(dt)
+
+	gl.BindTexture(gl.TEXTURE_2D, gameView.texture)
+	//oglEncap.SetTexture(console.Buffer()) //todo code the buffer
+	gameView.drawBuffer(gameView.ui.GetWindow())
+	gl.BindTexture(gl.TEXTURE_2D, 0)
+	if gameView.record {
+		//gameView.frames = append(gameView.frames, oglEncap.TakeScreenShot(console.Buffer())) todo
+	}
+}
+
+func (gameView *GameView) End() {
+	gameView.ui.GetWindow().SetKeyCallback(nil)
+	// view.console.SetAudioChannel(nil)
+	// view.console.SetAudioSampleRate(0)
+	// // save sram
+	// cartridge := view.console.Cartridge
+	// if cartridge.Battery != 0 {
+	// 	writeSRAM(sramPath(view.hash), cartridge.SRAM)
+	// }
+	// // save state
+	// view.console.SaveState(savePath(view.hash))
+}
+
+/** PRIVATE METHODS **/
+func (view *GameView) onKey(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if action == glfw.Press { // if key is pressed
+		switch key { //check which key has been pressed
+		//case glfw.KeySpace:
+		//	screenshot(view.console.Buffer())
+		case glfw.KeyR: // if i pressed r key i will restart my nes
+			view.nes.Reset()
+		case glfw.KeyTab:
+			if view.record {
+				view.record = false
+				oglEncap.Record(view.frames)
+				view.frames = nil
+			} else {
+				view.record = true
+			}
+		}
+	}
+}
+
+func (view *GameView) drawBuffer(window *glfw.Window) {
+	padding := 0
+	w, h := window.GetFramebufferSize()
+	s1 := float32(w) / 256
+	s2 := float32(h) / 240
+	f := float32(1 - padding)
+	var x, y float32
+	if s1 >= s2 {
+		x = f * s2 / s1
+		y = f
+	} else {
+		x = f
+		y = f * s1 / s2
+	}
+	gl.Begin(gl.QUADS)
+	gl.TexCoord2f(0, 1)
+	gl.Vertex2f(-x, -y)
+	gl.TexCoord2f(1, 1)
+	gl.Vertex2f(x, -y)
+	gl.TexCoord2f(1, 0)
+	gl.Vertex2f(x, y)
+	gl.TexCoord2f(0, 0)
+	gl.Vertex2f(-x, y)
+	gl.End()
 }
