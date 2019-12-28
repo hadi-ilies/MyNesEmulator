215a5547e4a612a5d20170f63c725b8bb2aaa4ae
hadi bereksi <hadi-ilies.bereksi-reguig@epitech.eu>
2019-12-28 16:08:23 +0100

    feat(ui): i get it ! now i know how golang struct works exactly private/public elem. i have added method getComponents on nes class

--- src/ui/nes.go
+++ src/ui/nes.go
@@ -4,8 +4,13 @@ import (
 	"github.com/hadi-ilies/MyNesEmulator/src/nescomponents"
 )
 
+/**
+MEMO : FIRST LETTER of struct elem DECIDE WETHER THE ELEM IS Private or public
+MAj -> public
+MIN -> private
+**/
 type Nes struct {
-	Bus *nescomponents.BUS
+	bus *nescomponents.BUS
 }
 
 func NewNes(gamePath string) Nes {
@@ -20,4 +25,9 @@ func (nes *Nes) Reset() {
 
 func (nes *Nes) Display() {
 
+}
+
+//get the circuit that is linked with all nes components
+func (nes *Nes) GetComponents() *nescomponents.BUS {
+	return nes.bus
 }
